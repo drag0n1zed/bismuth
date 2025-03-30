e843a2b13f37f1105bf16ab3690efd73fee8bd7b
drag0n1zed <drag0n1zed@outlook.com>
2025-03-30 22:01:08 +0800

    Seperate ItemTagGenerator into a new file

--- src/main/java/drag0n1zed/bismuth/ModDataGenerator.java
+++ src/main/java/drag0n1zed/bismuth/ModDataGenerator.java
@@ -1,10 +1,7 @@
 package drag0n1zed.bismuth;
+import drag0n1zed.bismuth.datagen.ItemTagGenerator;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
-import net.minecraft.registry.RegistryWrapper;
-import java.util.concurrent.CompletableFuture;
 
 public class ModDataGenerator implements DataGeneratorEntrypoint{
 
@@ -12,20 +9,6 @@ public class ModDataGenerator implements DataGeneratorEntrypoint{
     public void onInitializeDataGenerator(FabricDataGenerator generator) {
         FabricDataGenerator.Pack pack = generator.createPack();
 
-        pack.addProvider(TagGenerator::new);
-    }
-
-    private static class TagGenerator extends FabricTagProvider.ItemTagProvider {
-        public TagGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
-            super(output, completableFuture);
-        }
-
-        @Override
-        protected void configure(RegistryWrapper.WrapperLookup arg) {
-            // This creates a tag builder.
-            // This will automatically generate "src/main/generated/data/tutorial/tags/items/repairs_with_bismuth_crystal.json" in the "generated" folder.
-            getOrCreateTagBuilder(ModTags.REPAIRS_WITH_BISMUTH_CRYSTAL)
-                    .add(ModItems.BISMUTH_CRYSTAL);
-        }
+        pack.addProvider(ItemTagGenerator::new);
     }
 }

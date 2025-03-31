5ac64d8e7f74637d2303c2aa084166d88cfdea7f
drag0n1zed <drag0n1zed@outlook.com>
2025-03-31 21:35:08 +0800

    Revert to using json for translation for easier i18n

--- src/client/java/drag0n1zed/bismuth/client/ModDataGenerator.java
+++ src/client/java/drag0n1zed/bismuth/client/ModDataGenerator.java
@@ -3,7 +3,6 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 import drag0n1zed.bismuth.datagen.*;
-import drag0n1zed.bismuth.datagen.lang.*;
 import drag0n1zed.bismuth.client.datagen.*;
 
 public class ModDataGenerator implements DataGeneratorEntrypoint{
@@ -14,9 +13,5 @@ public class ModDataGenerator implements DataGeneratorEntrypoint{
 
         pack.addProvider(ItemTagProvider::new);
         pack.addProvider(ModelProvider::new);
-
-        // lang
-        pack.addProvider(EnUsProvider::new);
-        pack.addProvider(ZhCnProvider::new);
     }
 }

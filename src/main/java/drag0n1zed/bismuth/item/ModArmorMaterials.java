7ec60126aecc1d72a216ea793a54867c96e39da1
drag0n1zed <62168687+drag0n1zed@users.noreply.github.com>
2025-04-02 15:36:27 +0800

    Added bismuth armor material

---
+++ src/main/java/drag0n1zed/bismuth/item/ModArmorMaterials.java
@@ -0,0 +1,35 @@
+package drag0n1zed.bismuth.item;
+
+import drag0n1zed.bismuth.Bismuth;
+import drag0n1zed.bismuth.ModTags;
+import net.minecraft.item.equipment.ArmorMaterial;
+import net.minecraft.item.equipment.EquipmentAsset;
+import net.minecraft.item.equipment.EquipmentAssetKeys;
+import net.minecraft.item.equipment.EquipmentType;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Identifier;
+
+import java.util.Map;
+
+public class ModArmorMaterials {
+    public static final ArmorMaterial BISMUTH_ARMOR_MATERIAL = new ArmorMaterial(
+            15, // Base durability
+            Map.of(
+                    EquipmentType.HELMET, 3,
+                    EquipmentType.CHESTPLATE, 8,
+                    EquipmentType.LEGGINGS, 6,
+                    EquipmentType.BOOTS, 3
+            ),
+            5,
+            SoundEvents.ITEM_ARMOR_EQUIP_IRON,
+            0.0F,
+            0.0F,
+            ModTags.REPAIRS_WITH_BISMUTH,
+            keyOfEquipmentAsset("bismuth")
+    );
+
+    private static RegistryKey<EquipmentAsset> keyOfEquipmentAsset(String name){
+        return RegistryKey.of(EquipmentAssetKeys.REGISTRY_KEY, Identifier.of(Bismuth.MOD_ID,name));
+    }
+}
